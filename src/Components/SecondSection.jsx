27eeaab9db25bb47d4648f36cbd5fa2b97640b6c
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-10 13:48:57 +0100

    line-h

--- src/Components/SecondSection.jsx
+++ src/Components/SecondSection.jsx
@@ -2,9 +2,21 @@ import GarsettiFront from '../../src/assets/images/iFront 1.png'
 
 export default function SecondSection() {
   return (
-    <div className="bg-[#CECECE] min-h-screen px-[48px] pt-[42px] relative overflow-hidden flex items-center justify-center">
+    <div className="bg-[#CECECE] min-h-screen px-[48px] pt-[42px] relative overflow-hidden flex ">
       
-      {/* Background text */}
+
+      <div className='flex flex-col justify-between font-helvetica-neue-5'>
+                <div className='w-[40%] underline'>
+                    Back to top
+                </div>
+
+                <div className='w-[546px] text-3xl'>
+                    We believe you have a voice worth remembering just like every notable human that walked the earth.
+                    The mic for a notable voice.
+                </div>
+
+            </div>
+
 
       <div className='flex flex-col'>
 
@@ -16,21 +28,14 @@ export default function SecondSection() {
             </div>
 
 
-            <div className='w-[40%]'>
-                We believe you have a voice worth remembering just like every notable human that walked the earth.
-                The mic for a notable voice.
+      </div>
+           <div className="relative z-10 max-w-96">
+                <img 
+                src={GarsettiFront} 
+                alt="Garrett with microphone" 
+                className="object-cover w-full h-auto"
+                />
             </div>
-
-      </div>
-
-      {/* Foreground image */}
-      <div className="relative z-10 max-w-96">
-        <img 
-          src={GarsettiFront} 
-          alt="Garrett with microphone" 
-          className="object-cover w-full h-auto"
-        />
-      </div>
 
       
     </div>
