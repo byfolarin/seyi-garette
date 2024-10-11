adf24f1cf2f2ec4d64f10e3d0d2e4e2892ecc840
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-11 02:50:58 +0100

    midimages

--- src/Components/FifthSection.jsx
+++ src/Components/FifthSection.jsx
@@ -1,5 +1,7 @@
-import WomanMic from '../../src/assets/images/womanmic.png'
-import StudioMic from '../../src/assets/images/micstudio.png'
+import Mid1 from '../../src/assets/images/mid1.png'
+import Mid2 from '../../src/assets/images/mid2.png'
+import Mid3 from '../../src/assets/images/mid3.png'
+
 
 
 
@@ -24,45 +26,45 @@ export default function Fifth() {
                          <div className='w-1/3'>
                             <div className=" h-[528px] overflow-hidden">
                                 <img 
-                                src={StudioMic} 
+                                src={Mid1} 
                                 alt="Studio microphone" 
                                 className="w-full h-full object-cover"
                                 />
                             </div>
 
-                            <div>
-                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
-                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
+                                <h5 className='font-bold text-xl'>Crisp Audio Capture</h5>
+                                <p>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
                             </div>
                         </div>
 
                         <div className='w-1/3'>
                         <div className="h-[528px] overflow-hidden">
                                 <img 
-                                src={StudioMic} 
+                                src={Mid2} 
                                 alt="Studio microphone" 
                                 className="w-full h-full object-cover"
                                 />
                             </div>
 
-                            <div>
-                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
-                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
+                                <h5 className='font-bold text-xl'>Universal Port Compatibility</h5>
+                                <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
                             </div>
                         </div>
 
                         <div className='w-1/3'>
                         <div className="h-[528px] overflow-hidden">
                                 <img 
-                                src={StudioMic} 
+                                src={Mid3} 
                                 alt="Studio microphone" 
                                 className="w-full h-full object-cover"
                                 />
                             </div>
 
-                            <div>
-                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
-                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
+                                <h5 className='font-bold text-xl'>Universal Port Compatibility</h5>
+                                <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
                             </div>
                         </div>
 
