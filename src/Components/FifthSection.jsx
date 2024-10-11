f66dbb0758332898841bb7254ca34705e5105a28
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-11 02:52:41 +0100

    mid2

--- src/Components/FifthSection.jsx
+++ src/Components/FifthSection.jsx
@@ -34,7 +34,7 @@ export default function Fifth() {
 
                             <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
                                 <h5 className='font-bold text-xl'>Crisp Audio Capture</h5>
-                                <p>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
+                                <p className='font-helvetica-neue-5 text-base'>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
                             </div>
                         </div>
 
@@ -49,7 +49,7 @@ export default function Fifth() {
 
                             <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
                                 <h5 className='font-bold text-xl'>Universal Port Compatibility</h5>
-                                <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                                <p className='font-helvetica-neue-5 text-base'>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
                             </div>
                         </div>
 
@@ -64,7 +64,7 @@ export default function Fifth() {
 
                             <div className='pt-8 font-helvetica-neue-5 flex flex-col gap-1'>
                                 <h5 className='font-bold text-xl'>Universal Port Compatibility</h5>
-                                <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                                <p className='font-helvetica-neue-5 text-base'>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
                             </div>
                         </div>
 
