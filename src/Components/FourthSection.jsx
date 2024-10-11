5df7ab8c5b3e56dffaefe7168071c420fceec094
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-11 01:30:41 +0100

    align

--- src/Components/FourthSection.jsx
+++ src/Components/FourthSection.jsx
@@ -1,8 +1,18 @@
-
 export default function FourthSection() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+      <div className="px-[48px] pt-[42px]">
+        <div className="flex justify-between">
+
+          <h2 className="w-[45%] font-helvetica-neue-5 text-[32px] leading-tight">
+            Your words deserve to be remembered and voice deserves to be heard clearly. Whether you're podcasting or performing, GARSETTI® One captures your sound with stunning clarity, ensuring every word carries weight.
+          </h2>
+  
+          <div className='flex  justify-end align-middle items-start gap-8 mb-10 font-helvetica-neue-5 w-[45%]'>
+            <button className='py-[16px] px-[40px] bg-[#FF5931] rounded-full text-base whitespace-nowrap'>Join the Waitlist</button>
+            <button><a href="#" className='underline'>See what's in the box</a></button>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
