ab43680ab9a8c133a0ab542cf872d20419b67e67
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-11 02:41:10 +0100

    garsetti

--- src/Components/FifthSection.jsx
+++ src/Components/FifthSection.jsx
@@ -21,29 +21,52 @@ export default function Fifth() {
 
             <div className="flex gap-4 pt-[64px]">
 
-                        <div className="w-1/3 h-[528px] overflow-hidden">
-                            <img 
-                            src={WomanMic} 
-                            alt="Woman with microphone" 
-                            className="w-full h-full object-cover"
-                            />
+                         <div className='w-1/3'>
+                            <div className=" h-[528px] overflow-hidden">
+                                <img 
+                                src={StudioMic} 
+                                alt="Studio microphone" 
+                                className="w-full h-full object-cover"
+                                />
+                            </div>
+
+                            <div>
+                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
+                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            </div>
                         </div>
 
-                        <div className="w-1/3 h-[528px] overflow-hidden">
-                            <img 
-                            src={StudioMic} 
-                            alt="Studio microphone" 
-                            className="w-full h-full object-cover"
-                            />
+                        <div className='w-1/3'>
+                        <div className="h-[528px] overflow-hidden">
+                                <img 
+                                src={StudioMic} 
+                                alt="Studio microphone" 
+                                className="w-full h-full object-cover"
+                                />
+                            </div>
+
+                            <div>
+                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
+                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            </div>
                         </div>
 
-                        <div className="w-1/3 h-[528px] overflow-hidden">
-                            <img 
-                            src={StudioMic} 
-                            alt="Studio microphone" 
-                            className="w-full h-full object-cover"
-                            />
+                        <div className='w-1/3'>
+                        <div className="h-[528px] overflow-hidden">
+                                <img 
+                                src={StudioMic} 
+                                alt="Studio microphone" 
+                                className="w-full h-full object-cover"
+                                />
+                            </div>
+
+                            <div>
+                            <h5 className='font-medium text-xl'>Universal Port Compatibility</h5>
+                            <p>Adapt to any recording environment with ease. Our universal port supports a wide range of setups, ensuring you’re always ready to create, no matter the device or platform.</p>
+                            </div>
                         </div>
+
+                       
             </div>
 
 
