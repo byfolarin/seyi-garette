827917322094d553b463faf555d190a880decefc
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-15 19:31:50 +0100

    garette

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import HeroSection from "./Components/HeroSection"
-import SecondSection from "./Components/SecondSection"
-import ThirdSection from "./Components/ThirdSection"
-import FourthSection from "./Components/FourthSection"
-import Fifth from "./Components/FifthSection"
-import Sixth from "./Components/Sixth"
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import HeroSection from "./Components/HeroSection";
+import SecondSection from "./Components/SecondSection";
+import ThirdSection from "./Components/ThirdSection";
+import FourthSection from "./Components/FourthSection";
+import Fifth from "./Components/FifthSection";
+import Sixth from "./Components/Sixth";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => t,
+      smooth: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div>
       <HeroSection />
       <SecondSection />
       <ThirdSection />
-      <FourthSection/>
+      <FourthSection />
       <Fifth />
       <Sixth />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
