f219515d9bc017b1df6e1da1ac35e74724348e55
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-16 05:07:09 +0100

    first-section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -4,25 +4,27 @@ import GarretImg from '../../src/assets/images/Garsetti mic hero  1.png'
 export default function HeroSection() {
   return (
     <div className="bg-[#B8BBC2] min-h-screen px-[48px] pt-[42px] relative overflow-hidden">
+
       <div className='flex flex-col justify-between h-screen w-[40%]'>
-        <div className="relative z-10">
-          <p className="font-DepartureMono text-sm leading-tight text-[14px]">
+
+        <div className="relative z-10 w-[408px]">
+          <p className="font-DepartureMono text-sm leading-tight text-[12px]">
             FOR YEARS, OUR WORLD HAS BEEN TRANSFORMED BY SPEECHES FROM GREAT MEN WHO STOOD UP TO BE COUNTED. 
             THINK OF WHAT THEY SAID. THINK OF HOW THEY SAID IT. HERE'S YOUR WINDOW TO BEING REMEMBERED
           </p>
         </div>
         
-        <div className="relative z-10 mb-[48px] mt-[64px]">
-          <h1 className='text-5xl font-DepartureMono mb-10'>
+        <div className="relative z-10 mb-[48px] w-[408px]  mt-[64px]">
+          <h1 className='text-4xl font-DepartureMono mb-10'>
             ETCH YOUR VOICE INTO HISTORY 
           </h1>
           
-          <div className='flex gap-8 mb-10 font-helvetica-neue-5'>
-            <button  className='py-[16px] leading-none px-[40px] bg-[#FF5931] rounded-full text-base '>Join the Waitlist</button>
-            <button><a href="#" className='underline'>See what's in the box</a></button>
+          <div className='flex gap-4 mb-10 font-helvetica-neue-5'>
+            <button  className='py-[16px] leading-none px-[40px] bg-[#FF5931] rounded-full text-sm '>Join the Waitlist</button>
+            <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[8px]'>See what's in the box</a></button>
           </div>
 
-          <p className='font-base text-[#555963] font-helvetica-neue-5'>
+          <p className='text-sm text-[#555963]  font-helvetica-neue-5'>
             GARSETTI® brings you its flagship multi-dimensional microphone,
             designed to capture every nuance of your voice with precision.
           </p>
