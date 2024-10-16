57ee685d76ed25466169daca85736cf21b574776
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-16 05:22:56 +0100

    text-set

--- src/Components/FourthSection.jsx
+++ src/Components/FourthSection.jsx
@@ -5,18 +5,18 @@ import StudioMic from '../../src/assets/images/micstudio.png'
 
 export default function FourthSection() {
     return (
-      <div className="px-[48px] py-[42px]">
+      <div className="px-[48px] py-[92px]">
         <div className="flex justify-between">
 
-          <h2 className="w-[60%] font-helvetica-neue-5 text-[32px] leading-tight">
+          <h2 className="w-[50%] font-helvetica-neue-5 text-2xl leading-tight">
             Your words deserve to be remembered and voice deserves to be heard clearly. Whether you're podcasting or performing, GARSETTI® One captures your sound with stunning clarity, ensuring every word carries weight.
           </h2>
   
         <div className='flex justify-end align-middle items-start gap-8 mb-10 font-helvetica-neue-5 w-[45%]'>
 
             <div className="flex items-center gap-8">
-            <button className='py-[16px] px-[40px] bg-[#000000] rounded-full text-base text-white whitespace-nowrap'>Join the Waitlist</button>
-            <button><a href="#" className='underline'>See what's in the box</a></button>
+            <button  className='py-[16px] leading-none text-white px-[40px] bg-[#000000] rounded-full text-sm '>Join the Waitlist</button>
+            <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[8px]'>See what's in the box</a></button>
             </div>
         </div>
 
--- src/Components/ThirdSection.jsx
+++ src/Components/ThirdSection.jsx
@@ -4,16 +4,16 @@ export default function ThirdSection() {
   return (
     <div className="bg-[#CECECE] min-h-screen px-[48px] pt-[42px] relative overflow-hidden flex flex-col">
       
-      <div className='absolute top-[42px] left-[48px] font-helvetica-neue-5'>
+      {/* <div className='absolute top-[42px] left-[48px] font-helvetica-neue-5'>
         <div className='underline'>
           Back to top
         </div>
-      </div>
+      </div> */}
 
       <div className='flex-grow flex items-center justify-center relative'>
         {/* Background text */}
         <div className='absolute inset-0 flex justify-center items-center pointer-events-none'>
-          <div className='flex justify-center items-center tracking-tighter font-DepartureMono text-[180px] text-white opacity-50'>
+          <div className='flex justify-center items-center tracking-tighter font-DepartureMono text-[220px] text-white opacity-50'>
 
             <h1 className="-translate-y-40">GARSE</h1>
                  {/* <span className="w-[128px]"></span> */}
@@ -32,17 +32,17 @@ export default function ThirdSection() {
       </div>
 
       <div className='absolute bottom-[42px] left-[48px] right-[48px] font-helvetica-neue-5 flex justify-between'>
-        <div className='w-[446px] text-2xl leading-tight'>
+        <div className='w-[346px] text-xl leading-tight'>
           We believe you have a voice worth remembering just like every notable human that walked the earth.
           The mic for a notable voice.
         </div>
 
-        <div className='w-[446px] flex flex-col gap-8 '>
-          <p className='text-[#555963]'>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
+        <div className='w-[346px]  flex flex-col gap-8 '>
+          <p className='text-[#555963] text-sm'>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
           
-          <div className='flex gap-8 mb-10 font-helvetica-neue-5 justify-end '>
-            <button className='py-[16px] px-[40px] bg-[#FF5931] rounded-full text-base'>Join the Waitlist</button>
-            <button><a href="#" className='underline'>See what's in the box</a></button>
+          <div className='flex gap-4 mb-10 font-helvetica-neue-5'>
+            <button  className='py-[16px] leading-none px-[40px] bg-[#FF5931] rounded-full text-sm '>Join the Waitlist</button>
+            <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[8px]'>See what's in the box</a></button>
           </div>
         </div>
       </div>
