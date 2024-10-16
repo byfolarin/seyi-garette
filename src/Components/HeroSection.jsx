883985d073cecd5068189587c4c4f4bba1d522ef
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-17 00:54:05 +0100

    responsiveness

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import GarretImg from '../../src/assets/images/Garsetti mic hero  1.png'
+import GarretMobile from '../../src/assets/images/Garsetti-mobile.png'
+
+
 
 export default function HeroSection() {
   return (
-    <div className="bg-[#B8BBC2] min-h-screen px-[48px] pt-[42px] relative overflow-hidden">
+    <div className="bg-[#B8BBC2] min-h-screen px-[16px] lg:px-[48px] pt-[42px] relative overflow-hidden">
 
       <div className='flex flex-col justify-between h-screen w-[40%]'>
 
         <div className="relative z-10 w-[408px]">
-          <p className="font-DepartureMono text-sm leading-tight text-[12px]">
+          <p className="font-DepartureMono absolute text-sm leading-tight text-[12px]">
             FOR YEARS, OUR WORLD HAS BEEN TRANSFORMED BY SPEECHES FROM GREAT MEN WHO STOOD UP TO BE COUNTED. 
             THINK OF WHAT THEY SAID. THINK OF HOW THEY SAID IT. HERE'S YOUR WINDOW TO BEING REMEMBERED
           </p>
         </div>
+
+
+        <div className="w-[451px] absolute md:hidden border border-red-600 lg:hidden">
+        <img 
+          src={GarretMobile} 
+          alt="Garrett with microphone" 
+          className="object-cover w-full h-full"
+        />
+      </div>
         
         <div className="relative z-10 mb-[48px] w-[608px]  mt-[64px]">
 
-            <h1 className='text-5xl leading-[128%] font-DepartureMono mb-10'>
+            <h1 className='text-5xl border border-red-700 leading-[128%] font-DepartureMono mb-10'>
               ETCH YOUR VOICE INTO HISTORY 
             </h1>
+
+
             
-            <div className='flex gap-4 mb-10 font-helvetica-neue-5'>
-              <button  className='py-[17px] leading-none px-[40px] bg-[#FF5931]  rounded-full text-sm '>Join the Waitlist</button>
+        <div className='flex gap-4 mb-10 font-helvetica-neue-5'>
+          <button  className='py-[17px] leading-none px-[40px] bg-[#FF5931]  rounded-full text-sm '>Join the Waitlist</button>
               <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[4px]'>See what's in the box</a></button>
             </div>
 
@@ -35,7 +49,7 @@ export default function HeroSection() {
 
       </div>
       
-      <div className="absolute top-0 right-0 w-[80%] h-full overflow-hidden">
+      <div className="hidden lg:block lg:absolute lg:top-0 lg:right-0 lg:w-[80%] lg:h-full lg:overflow-hidden">
         <img 
           src={GarretImg} 
           alt="Garrett with microphone" 
