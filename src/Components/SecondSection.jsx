ab014ab7c84e806b2a555b4e4c6031696d18915b
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-17 03:42:02 +0100

    second-sec

--- src/Components/SecondSection.jsx
+++ src/Components/SecondSection.jsx
@@ -15,7 +15,7 @@ export default function SecondSection() {
         {/* Background text */}
         <div className='lg:absolute lg:inset-0 flex justify-center items-center pointer-events-none'>
 
-          <div className='flex flex-col lg:flex-row justify-center items-center tracking-tighter font-DepartureMono text-[92px] lg:text-[220px] text-white opacity-50'>
+          <div className='lg:flex flex-col lg:flex-row justify-center items-center tracking-tighter font-DepartureMono text-[92px] lg:text-[220px] hidden text-white opacity-50'>
 
             <h1 className="lg:-translate-y-40 leading-[80%]">GARSE <span className='lg:hidden'>TTI1</span></h1>
                  {/* <span className="w-[128px]"></span> */}
@@ -39,7 +39,7 @@ export default function SecondSection() {
 
         <div className='lg:absolute lg:inset-0 flex justify-center items-center pointer-events-none'>
 
-            <div className='flex flex-col lg:flex-row justify-center items-center tracking-tighter font-DepartureMono text-[92px] lg:text-[220px] text-white opacity-50'>
+            <div className='flex flex-col lg:flex-row justify-center items-center tracking-tighter font-DepartureMono text-[92px] lg:text-[220px] ml-6 lg:ml-0 text-white opacity-50'>
 
               <h1 className="lg:-translate-y-40 leading-[80%]">GARSE <span className='lg:hidden'>TTI1</span></h1>
                   {/* <span className="w-[128px]"></span> */}
@@ -52,13 +52,13 @@ export default function SecondSection() {
 
       </div>
 
-      <div className='lg:absolute bottom-[42px] left-[48px] right-[48px] font-helvetica-neue-5 flex flex-col lg:flex-row justify-between'>
+      <div className='lg:absolute bottom-[42px] pt-[52px] lg:pt-0 left-[48px] right-[48px] font-helvetica-neue-5 flex flex-col lg:flex-row justify-between'>
         <div className='w-[346px] text-xl leading-tight'>
           We believe you have a voice worth remembering just like every notable human that walked the earth.
           The mic for a notable voice.
         </div>
 
-        <div className='w-[346px] flex flex-col gap-8 '>
+        <div className='w-[346px] flex flex-col gap-8 pt-[72px] lg:pt-0'>
           <p className='text-[#555963] text-sm'>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
           
           <div className='flex flex-col lg:flex-row gap-4 mb-10 font-helvetica-neue-5'>
