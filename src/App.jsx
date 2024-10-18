bbef56f859c368c095bbae8f4a6f6b62e62898b7
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-18 03:31:13 +0100

    7th

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
       <Fifth />
       <Sixth />
       <SeventhSection />
-      {/* <EightSection /> */}
+      <EightSection />
     </div>
   );
 }
--- src/Components/EightSection.jsx
+++ src/Components/EightSection.jsx
@@ -4,13 +4,15 @@ export default function EightSection() {
   return (
 
     <div className='relative'>
+
+
             <div className=' bg-[#FF5931] absolute rounded-b-[80px] z-20 w-full  font-helvetica-neue-5 px-[49px]'>                 
                 <div className="flex flex-col gap-6 items-center py-[110px] w-full mx-auto justify-center">             
                         <h3 className="w-[70%] text-2xl  text-center">
                             Choose more than just a tool – choose to create something timeless. <br /> Choose GARSETTI One
                         </h3>
 
-                        <div className="flex items-center gap-8 ">
+                        <div className="flex flex-col lg:flex-row items-center gap-8 ">
                                 <button  className='py-[17px] leading-none  px-[40px] text-white bg-[#000000] rounded-full text-sm '>Join the Waitlist</button>
                                 <button><a href="#" className=' border-black text-black border-b-[1.5px] text-sm py-[8px]'>Contact the Designer</a></button>
                         </div>
