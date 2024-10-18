b5bc72c41abfc976c04b0e8819e71ee8cd945c73
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-18 01:36:06 +0100

    done

--- src/Components/ThirdSection.jsx
+++ src/Components/ThirdSection.jsx
@@ -29,6 +29,7 @@ export default function SecondSection() {
           />
         </div>
 
+
         <div className='md:absolute lg:absolute md:inset-0 lg:inset-0 flex justify-center items-center pointer-events-none'>
           <div className='flex flex-col md:flex-row lg:flex-row justify-center items-center tracking-tighter font-DepartureMono text-[92px] md:text-[180px] lg:text-[220px] ml-6 md:ml-0 lg:ml-0 text-white opacity-50'>
             <h1 className="md:-translate-y-20 lg:-translate-y-40 leading-[80%]">GARSE <span className='sm:flex md:hidden lg:hidden'>TTI1</span></h1>
@@ -37,21 +38,31 @@ export default function SecondSection() {
         </div>
       </div>
 
+
       <div className='md:absolute lg:absolute bottom-[42px] pt-[52px] md:pt-0 lg:pt-0 md:left-[32px] md:right-[32px] lg:left-[48px] lg:right-[48px] font-helvetica-neue-5 flex flex-col md:flex-row lg:flex-row justify-between'>
+
+
         <div className='w-full md:w-[300px] lg:w-[346px] text-xl leading-tight mb-8 md:mb-0 lg:mb-0'>
           We believe you have a voice worth remembering just like every notable human that walked the earth.
           The mic for a notable voice.
         </div>
 
+
         <div className='w-full md:w-[300px] lg:w-[346px] flex flex-col gap-8 pt-[72px] md:pt-0 lg:pt-0'>
           <p className='text-[#555963] text-sm'>Experience not just an engineering marvel but an artistic one as well. The GARSETTI One seeks to further revolutionise audio capturing. </p>
+
           
           <div className='flex flex-col md:flex-row lg:flex-row gap-4 mb-10 font-helvetica-neue-5'>
             <button className='py-[17px] leading-none px-[40px] bg-[#FF5931] text-nowrap rounded-full text-sm'>Join the Waitlist</button>
             <button><a href="#" className='border-black border-b-[1.5px] text-nowrap text-sm py-[8px]'>See what's in the box</a></button>
           </div>
+          
         </div>
+
       </div>
+
+
+
     </div>
   )
 }
