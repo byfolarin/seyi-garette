9807e91d4125531a653edc056410c493feb4e448
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-18 01:59:23 +0100

    selah

--- src/Components/FourthSection.jsx
+++ src/Components/FourthSection.jsx
@@ -8,16 +8,16 @@ export default function FourthSection() {
       <div className="px-[16px] lg:px-[48px] py-[92px]">
 
 
-        <div className="flex flex-col lg:flex-row md:flex-row justify-between">
+        <div className="flex flex-col gap-6 lg:flex-row md:flex-row justify-between">
 
-                <h2 className="w-[50%] font-helvetica-neue-5 md:text-2xl text-xl lg:text-2xl leading-tight">
+                <h2 className="w-full md:w-[50%] lg:w-[50%] font-helvetica-neue-5 md:text-2xl text-xl lg:text-2xl leading-tight">
                   Your words deserve to be remembered and voice deserves to be heard clearly. Whether you're podcasting or
                    performing, GARSETTI® One captures your sound with stunning clarity, ensuring every word carries weight.
                 </h2>
         
-              <div className='flex justify-end align-middle items-start gap-8 mb-10 font-helvetica-neue-5 w-[45%]'>
+              <div className='flex flex-col lg:flex-row md:flex-row lg:justify-end align-middle items-start gap-8 mb-10 font-helvetica-neue-5 w-full lg:w-[45%]'>
 
-                  <div className="flex flex-col lg:flex-row md:flex-row items-center gap-8">
+                  <div className="flex flex-col  lg:flex-row md:flex-row md:items-center lg:items-center gap-8">
                       <button  className='py-[17px] leading-none text-white px-[40px] bg-[#000000] text-nowrap rounded-full text-sm '>Join the Waitlist</button>
                       <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[8px]'>See what's in the box</a></button>
                   </div>
