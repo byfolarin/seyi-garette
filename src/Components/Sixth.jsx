1f385b57057431481de2fc046e55aa5e5305a249
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-21 03:41:56 +0100

    see

--- src/Components/Sixth.jsx
+++ src/Components/Sixth.jsx
@@ -7,18 +7,31 @@ import Tick from '../../src/assets/images/Vector 4.svg'
 
 export default function Sixth() {
     return (
-        <div className="relative z-[24] px-[16px] md:px[32px] lg:px-[48px] py-[68px] bg-white">
+        <div className="relative z-[24] px-[16px] md:px[32px] lg:px-[48px] py-[68px] font-helvetica-neue-5 bg-white">
         <div className="flex flex-collg:flex-row gap-6">
 
 
                 <div className='w-full lg:w-[70%] flex flex-col lg:flex-row gap-12'>
+                    
                     <div className='flex items-start w-full font-helvetica-neue-5 text-[#555963]'>
-                     <h4 className='border-l-[3px] pl-6 font-medium border-[#E5E5E5]'>Check out what’s <br /> in the box</h4>   
-                 </div>
+                        <h4 className='border-l-[3px] pl-6 font-medium border-[#E5E5E5]'>Check out what’s <br /> in the box</h4>   
+                    </div>
 
-                    <h2 className="font-helvetica-neue-5 text-2xl leading-tight ">
-                    Complete your GARSETTI One microphone setup with our range of high-performance accessories. Each accessory is designed to enhance your sound and make your recording process seamless.
+
+                <div className='flex flex-col justify-between'>
+
+                     <h2 className="font-helvetica-neue-5 text-2xl leading-tight ">
+                        Complete your GARSETTI One microphone setup with our range of high-performance accessories. Each accessory is designed to enhance your sound and make your recording process seamless.
                     </h2>
+
+
+                    <div className="flex flex-col lg:flex-row   md:flex-row   md:items-center lg:items-center gap-8">
+                        <button  className='py-[17px] leading-none text-white px-[40px] bg-[#000000] text-nowrap rounded-full text-sm '>Join the Waitlist</button>
+                        <button><a href="#" className=' border-black border-b-[1.5px] text-sm py-[8px]'>Contact the designer</a></button>
+                    </div>
+
+                </div>
+                    
 
                   
 
