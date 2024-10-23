a6fa8c8a659455ae1fea0b9f9d9b76d9a88a0d94
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-23 10:57:09 +0100

    transaction

--- src/Components/FifthSection.jsx
+++ src/Components/FifthSection.jsx
@@ -9,7 +9,7 @@ export default function Fifth() {
         show: {
             opacity: 1,
             transition: {
-                staggerChildren: 0.2,
+                staggerChildren: 0.6,
                 delayChildren: 0.6
             }
         }
