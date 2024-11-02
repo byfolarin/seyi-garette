a0331dcb0437b3280f32e3db73ec0d4bfc30fd37
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-02 23:26:41 +0100

    te

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -13,7 +13,6 @@ export default function HeroSection() {
   const frame50Rotate = useTransform(scrollY, [0, 1000], [40, 0]);
   const [showSignUp, setShowSignUp] = useState(false);
 
-
   const handleClose = () => {
     setShowSignUp(false);
   };
@@ -30,7 +29,8 @@ export default function HeroSection() {
       </div>
 
       <div className="bg-[#B8BBC2] h-screen px-4 sm:px-6 lg:px-12 pt-6 lg:pt-[42px] relative overflow-hidden">
-        <div className='mx-auto max-w-[1440px] relative'>
+        <div className='mx-auto max-w-[1440px] relative h-full flex flex-col'>
+          {/* Top section with typewriter and navigation */}
           <div className='flex justify-between items-start'>
             <div className="relative font-DepartureMono text-[12px] z-10 w-full sm:w-[408px] mb-6 lg:mb-0 h-[100px]">
               <Typewriter
@@ -61,81 +61,81 @@ export default function HeroSection() {
             </div>
           </div>
 
-          <div className='flex flex-col justify-between min-h-screen w-full lg:w-[60%]'>   
-            <div className="lg:hidden w-full sm:w-[451px] mt-6">
-              <img 
-                src={GarretMobile} 
-                alt="Garrett with microphone" 
-                className="object-cover w-full"
-              />
-            </div>
+          {/* Mobile image */}
+          <div className='lg:hidden w-full sm:w-[451px] mt-6'>
+            <img 
+              src={GarretMobile} 
+              alt="Garrett with microphone" 
+              className="object-cover w-full"
+            />
+          </div>
 
-            <div className="relative z-[9999] mb-8 lg:mb-8 w-full lg:w-[608px] mt-6 lg:mt-16">
+          {/* Bottom section with heading and content */}
+          <div className="mt-auto mb-8 relative z-[9999] w-full lg:w-[608px]">
             {showSignUp ? (
-               <SignUpModal onClose={handleClose} /> 
-            ): (
-                <div className='flex flex-col'>       
-                  <div className='flex items-center gap-4  mb-[16px]'>
-                    <div className='w-[14px] h-[14px] bg-[#FC451A] overflow-hidden cursor-pointer rounded-full'></div>
-                    <div className='flex items-center w-[248px] h-[24px] relative'>
-                      <Marquee
-                        speed={40}
-                        direction='right'
-                        gradient={false}
-                      >
-                        <p className='font-DepartureMono text-[12px] whitespace-nowrap'>
-                          I HAVE A DREAM - MARTIN LUTHER
-                        </p> 
-                        <span className='w-8'></span>
-                      </Marquee>
-                    </div>
-                  </div>
-
-                  <h1 className='text-4xl sm:text-5xl leading-tight lg:leading-[128%] font-DepartureMono mb-4 lg:mb-4'>
-                    ETCH YOUR VOICE <br /> INTO HISTORY 
-                  </h1>
-                  
-                  <div className='flex flex-col sm:flex-row gap-8 mb-4 lg:mb-10 font-helvetica-neue-5'>
-                  <button 
-                      onClick={() => setShowSignUp(true)} 
-                      className="flex items-center justify-center h-[56px] cursor-pointer leading-[1.5] px-8 lg:px-[40px] bg-[#FF5931] rounded-full text-sm whitespace-nowrap"
+              <SignUpModal onClose={handleClose} />
+            ) : (
+              <div className='flex flex-col'>       
+                <div className='flex items-center gap-4 mb-[16px]'>
+                  <div className='w-[14px] h-[14px] bg-[#FC451A] overflow-hidden cursor-pointer rounded-full'></div>
+                  <div className='flex items-center w-[248px] h-[24px] relative'>
+                    <Marquee
+                      speed={40}
+                      direction='right'
+                      gradient={false}
                     >
-                      Join the Waitlist
-                    </button>
-
-                    <motion.button 
-                      className="py-1 sm:py-0 relative group leading-normal"
-                      whileHover="hover"
-                      initial="rest"
-                      animate="rest"
-                    >
-                      <a href="#" className="text-sm py-1 hover:text-[#3d3d3d] relative inline-block leading-normal">
-                        See what's in the box
-                        <motion.span
-                          className="absolute bottom-0 left-0 h-[1px] bg-black w-full"
-                          variants={{
-                            rest: { width: '100%', left: '0%' },
-                            hover: { 
-                              width: ['100%', '0%', '0%', '100%'],
-                              left: ['0%', '100%', '0%', '0%'],
-                              transition: { 
-                                duration: 0.4,
-                                times: [0, 0.4, 0.5, 1]
-                              }
-                            }
-                          }}
-                        />
-                      </a>
-                    </motion.button>               
+                      <p className='font-DepartureMono text-[12px] whitespace-nowrap'>
+                        I HAVE A DREAM - MARTIN LUTHER
+                      </p> 
+                      <span className='w-8'></span>
+                    </Marquee>
                   </div>
-
-                  <p className='text-sm text-[#555963] w-full sm:w-[420px] font-helvetica-neue-5'>
-                    GARSETTI® brings you its flagship multi-dimensional microphone,
-                    designed to capture every nuance of your voice with precision.
-                  </p>
                 </div>
-              )}
-            </div>
+
+                <h1 className='text-4xl sm:text-5xl leading-tight lg:leading-[128%] font-DepartureMono mb-4 lg:mb-4'>
+                  ETCH YOUR VOICE <br /> INTO HISTORY 
+                </h1>
+                
+                <div className='flex flex-col sm:flex-row gap-8 mb-4 lg:mb-10 font-helvetica-neue-5'>
+                  <button 
+                    onClick={() => setShowSignUp(true)} 
+                    className="flex items-center justify-center h-[56px] cursor-pointer leading-[1.5] px-8 lg:px-[40px] bg-[#FF5931] rounded-full text-sm whitespace-nowrap"
+                  >
+                    Join the Waitlist
+                  </button>
+
+                  <motion.button 
+                    className="py-1 sm:py-0 relative group leading-normal"
+                    whileHover="hover"
+                    initial="rest"
+                    animate="rest"
+                  >
+                    <a href="#" className="text-sm py-1 hover:text-[#3d3d3d] relative inline-block leading-normal">
+                      See what's in the box
+                      <motion.span
+                        className="absolute bottom-0 left-0 h-[1px] bg-black w-full"
+                        variants={{
+                          rest: { width: '100%', left: '0%' },
+                          hover: { 
+                            width: ['100%', '0%', '0%', '100%'],
+                            left: ['0%', '100%', '0%', '0%'],
+                            transition: { 
+                              duration: 0.4,
+                              times: [0, 0.4, 0.5, 1]
+                            }
+                          }
+                        }}
+                      />
+                    </a>
+                  </motion.button>               
+                </div>
+
+                <p className='text-sm text-[#555963] w-full sm:w-[420px] font-helvetica-neue-5'>
+                  GARSETTI® brings you its flagship multi-dimensional microphone,
+                  designed to capture every nuance of your voice with precision.
+                </p>
+              </div>
+            )}
           </div>
         </div>
         
