46b53c4113193b9829fc1ebe505f71bceb52cf58
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-02 22:22:50 +0100

    helvetica-neue

--- src/Components/SignUpModal.jsx
+++ src/Components/SignUpModal.jsx
@@ -9,8 +9,8 @@ const SignUpModal = () => {
         </div>
 
 
-        <div className='flex items-center'>
-            <p>Enter your email address</p>
+        <div className='flex items-center font-helvetica-neue-5 justify-between w-[476px] bg-[#FF5931] py-[12px] pr-[12px] pl-[32px] rounded-full'>
+            <p className=' text-base'>Enter your email address</p>
             <button className='px-[23px] py-[16px] border rounded-full border-[#333333]'>Submit</button>
         </div>
    
