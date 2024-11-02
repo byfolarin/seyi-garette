5e25134e9f3485a37e5fea6a08a827625ec399a8
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-02 22:19:06 +0100

    btn

--- src/Components/SignUpModal.jsx
+++ src/Components/SignUpModal.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const SignUpModal = () => {
   return (
     <div>
-        <div>
+        <div className='flex justify-between'>
                 <h2 className='text-[36px] font-helvetica-neue-5'>Join the waitlist</h2>
                 <p className='font-helvetica-neue-5 text-base'>Close</p>
+        </div>
+
+
+        <div className='flex items-center'>
+            <p>Enter your email address</p>
+            <button className='px-[23px] py-[16px] border rounded-full border-[#333333]'>Submit</button>
         </div>
    
     </div>
