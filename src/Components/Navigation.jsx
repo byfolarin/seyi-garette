cc44a130199f02f6a646d338459868f4f64ffdb9
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 10:09:20 +0100

    restful

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div>
-      <div>Section 01:</div>
-      <div>Our belief of your voice</div>
+    <div className='flex'>
+
+        <div className='flex'>
+                <div>Section 01:</div>
+                <div>Our belief of your voice</div>
+        </div>
+     
 
 
       <div>Back to top</div>
