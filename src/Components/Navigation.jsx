28f3bff787bde52bde060b258932944d36d700eb
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 09:56:05 +0100

    waitlist

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Navigation() {
   return (
     <div>
-      Hello World
+      <div>Section 01:</div>
+      <div>Our belief of your voice</div>
+
+
+      <div>Back to top</div>
+
+      <button>Join the Waitlist</button>
     </div>
   )
 }
