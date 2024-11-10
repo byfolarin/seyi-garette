52fefa1af1fe52301c840f2ac062d0d2bd621c48
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 10:17:35 +0100

    bg-blur

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div className='flex'>
+    <div className='flex w-[705px] gap-8 py-[24px] px-[32px] text-white backdrop-blur-[96] rounded-full bg-[#1F1F1F] opacity-50 border border-[#0000001e]'>
 
-        <div className='flex'>
-            <div>Section 01:</div>
-            <div>Our belief of your voice</div>
-        </div>
-     
-        <div>Back to top</div>
+            <div className='flex'>
+                <div>Section 01:</div>
+                <div>Our belief of your voice</div>
+            </div>
+        
+            <div>Back to top</div>
 
-        <button>Join the Waitlist</button>
-
+            <button>Join the Waitlist</button>
 
     </div>
   )
