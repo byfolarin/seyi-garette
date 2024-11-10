a470792374a5073c4ee9bbbdbf91adbcadbfe9e3
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 12:28:53 +0100

    py

--- src/Components/NavCompo.jsx
+++ src/Components/NavCompo.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 export default function NavCompo() {
   return (
-    <div>
+    <div className='font-helvetica-neue-5'>
+        <div className='py-[16px]'>
+                <ul>
+                <li>Etch your voice</li>
+                <li>Our belief of your voice</li>
+                <li>Your words deserve</li>
+                <li>The GARSETTI Marvel</li>
+                <li>What’s in the box?</li>
+                <li>What you’ve just witnessed</li>
+                <li>Footer | Closing</li>
+            </ul>
+        </div>
       
     </div>
   )
