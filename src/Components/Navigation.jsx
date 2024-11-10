7495d899341424952e88930f015257a77cc1473e
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 10:28:40 +0100

    flow

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 pb-[42px]">
+    <div className="fixed bottom-0 left-0 right-0 z-50 pb-[42px] font-helvetica-neue-5">
       <div className='flex w-[705px] mx-auto justify-between py-[24px] 
-      px-[32px] text-white backdrop-blur-[96] rounded-full bg-[#1F1F1F] opacity-50 border border-[#0000001e]'>
+      px-[32px] text-white backdrop-blur-lg rounded-full bg-[#1F1F1F]/50 border border-[#0000001e]'>
         <div className='flex'>
           <div>Section 01:</div>
           <div>Our belief of your voice</div>
