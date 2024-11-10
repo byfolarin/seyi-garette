a3cd08161f1fe56204ea10b5a07bad1d134139c3
Folarin Folarin <thefolafolarin@gmail.com>
2024-11-10 10:26:16 +0100

    section

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export default function Navigation() {
+const Navigation = () => {
   return (
-    <div className='flex w-[705px] gap-8 py-[24px] px-[32px] text-white backdrop-blur-[96] rounded-full bg-[#1F1F1F] opacity-50 border border-[#0000001e]'>
+    <div className="fixed bottom-0 left-0 right-0 z-50 pb-[42px]">
+      <div className='flex w-[705px] mx-auto justify-between py-[24px] 
+      px-[32px] text-white backdrop-blur-[96] rounded-full bg-[#1F1F1F] opacity-50 border border-[#0000001e]'>
+        <div className='flex'>
+          <div>Section 01:</div>
+          <div>Our belief of your voice</div>
+        </div>
+    
+        <div>Back to top</div>
 
-            <div className='flex'>
-                <div>Section 01:</div>
-                <div>Our belief of your voice</div>
-            </div>
-        
-            <div>Back to top</div>
-
-            <button>Join the Waitlist</button>
-
+        <button>Join the Waitlist</button>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Navigation;
