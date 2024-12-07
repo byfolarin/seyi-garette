bf799fa71fed86f3b13f53eb94641d24108aa799
Folarin Folarin <thefolafolarin@gmail.com>
2024-12-07 22:38:48 +0100

    jsx

--- src/Components/Frame50.jsx
+++ src/Components/Frame50.jsx
@@ -1,9 +1,14 @@
-import Frame5 from '../assets/images/Frame 50.png'
+import React from 'react';
+import Frame5 from '../assets/images/Frame 50.png';
 
 export default function Frame50() {
   return (
-    <div className='w-[694px]'>
-      <img src={Frame5} alt="" />
+    <div className='w-full max-w-[694px] px-4 mx-auto'>
+      <img 
+        src={Frame5} 
+        alt="" 
+        className='w-full h-auto object-contain'
+      />
     </div>
   )
 }
