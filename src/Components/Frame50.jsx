b47a63c6738320c58a928fc58c8478fe7bc3bf9d
Folarin Folarin <thefolafolarin@gmail.com>
2024-12-13 12:16:53 +0100

    frames

--- src/Components/Frame50.jsx
+++ src/Components/Frame50.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Frame5 from '../assets/images/Frame 50.png';
+// import React from 'react';
+// import Frame5 from '../assets/images/Frame 50.png';
 
-export default function Frame50() {
-  return (
-    <div className='w-full max-w-[294px] sm:max-w-[394px] md:max-w-[494px] lg:max-w-[694px] px-4 mx-auto'>
-      <img 
-        src={Frame5} 
-        alt="" 
-        className='w-full h-auto object-contain'
-      />
-    </div>
-  )
-}
+// export default function Frame50() {
+//   return (
+//     <div className='w-full max-w-[294px] sm:max-w-[394px] md:max-w-[494px] lg:max-w-[694px] px-4 mx-auto'>
+//       <img 
+//         src={Frame5} 
+//         alt="" 
+//         className='w-full h-auto object-contain'
+//       />
+//     </div>
+//   )
+// }
