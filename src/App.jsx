7231bce5217d8896d977c45a1f71d1d4ef02960b
Folarin Folarin <thefolafolarin@gmail.com>
2025-02-09 23:52:28 +0100

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ function App() {
           <SeventhSection />
         </section>
 
-        {/* Eighth Section */}
+        {/* Eighth Sgction */}
         <section 
           id="section8" 
           ref={eighthRef}
